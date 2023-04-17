bb44f3e78ade6a177fe0a4e8144631e02e40f78c
DergayPavel <53352812+DergayPavel@users.noreply.github.com>
2023-04-17 23:58:32 +0300

    feat: add repeater  function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  if (Object.keys(options).length > 0) {
+    let addArr = [];
+    let newAdd = '';
+    let arr = [];
+    
+    if (options.hasOwnProperty('addition')) {
+      let stringAdd = String(options.addition);
+
+      if (!options.additionRepeatTimes) {
+        addArr.push(stringAdd);
+      } else {
+        for (let i = 0; i < options.additionRepeatTimes; i++) {
+          addArr.push(stringAdd);
+        }
+      }
+      if(options.additionSeparator ){
+        newAdd = addArr.join(options.additionSeparator)
+      }
+      else{
+         newAdd = addArr.join('|')
+      }
+    }
+
+    if (!options.repeatTimes) {
+      arr.push(str + newAdd);
+    } else {
+      for (let i = 0; i < options.repeatTimes; i++) {
+        arr.push(str + newAdd);
+      }
+    }
+    
+
+    return arr.join(options.separator ? options.separator : '+');
+
+  } else {
+    return str;
+  }
 }
 
 module.exports = {
