9b6b0c0219a1045190475a542fbcd51e5f8e9884
DergayPavel <53352812+DergayPavel@users.noreply.github.com>
2023-04-17 20:08:41 +0300

    feat: add matrix-elements and mac-address function

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,8 +16,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  
+function getMatrixElementsSum(matrix) {
+  let summ=0;
+  for(let n=0; n<matrix.length;n++){
+    for(let m=0; m<matrix[n].length; m++){
+      let check=false;
+      for(let i=0; i<n; i++){
+        if(matrix[i][m]===0){
+          check=true
+        }
+      }
+      if(!check){
+        summ+=matrix[n][m]
+      }
+    }
+  }
+  return summ
 }
 
 module.exports = {
